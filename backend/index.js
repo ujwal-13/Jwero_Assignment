103852eb437ef9c460cf750ce834c4ba21ff0361
ujwal-13 <ujwalt13@gmail.com>
2026-01-05 20:59:32 +0530

    feat: set up MySQL connection pool with environment variables

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const analyticsRoutes = require('./config/routes/analyticsRoutes');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/analytics', analyticsRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+});
