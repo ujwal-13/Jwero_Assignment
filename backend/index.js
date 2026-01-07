559d4e9e2a0ece6c3362d53c920f22373873076e
ujwal-13 <ujwalt13@gmail.com>
2026-01-07 21:29:12 +0530

    updated db.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Routes
 app.use('/api/analytics', analyticsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(` Server running on port ${PORT}`);
 });
