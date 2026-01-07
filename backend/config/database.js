1b3ea773c7e1e1ae1a2c749fd041c820e3281476
ujwal-13 <ujwalt13@gmail.com>
2026-01-07 15:22:58 +0530

    modified backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const urlDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`
-const pool =mysql.createConnection(urlDB);
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
+const pool =mysql.createPool(urlDB);
 
 // const pool = mysql.createPool({
 //     host: process.env.DB_HOST,
@@ -13,5 +13,6 @@ const pool =mysql.createConnection(urlDB);
 //     connectionLimit: 10,
 //     queueLimit: 0
 // });
+
 
 module.exports = pool;
